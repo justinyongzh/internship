da0455a858efe1548357fdfea9362970eee7f023
diongzy <86460920+diongzy@users.noreply.github.com>
2023-09-21 13:07:29 +0800

    Update InternApp.py

--- InternApp.py
+++ InternApp.py
@@ -26,6 +26,9 @@ db_conn = connections.Connection(
 
 )
 
+output = {}
+table = 'Student'
+
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -197,6 +200,51 @@ def displayStudentResume(stud_id):
 
 #     print("all modification done...")
 #     return render_template('AddEmpOutput.html', name=emp_name)
+
+
+
+
+@app.route("/studProfile/<stud_id>", methods=['GET', 'POST'])
+def GetStudInfo(stud_id):
+    # Fetch student information from the database
+    cursor = db_conn.cursor()
+    cursor.execute(f"SELECT * FROM Student WHERE stud_id = {stud_id}")
+    student_data = cursor.fetchone()
+    cursor.close()
+
+    if student_data:
+        # Extract student information
+        stud_id, stud_name, stud_gender, stud_IC, stud_email, stud_HP, stud_address, stud_programme, stud_resume = student_data
+
+        # Create a link to download the resume
+        resume_link = f"/preview/{stud_id}"
+
+        return render_template('studProfile.html', stud_id=stud_id, stud_name=stud_name, stud_gender=stud_gender,
+                               stud_IC=stud_IC, stud_email=stud_email, stud_HP=stud_HP, stud_address=stud_address,
+                               stud_programme=stud_programme, resume_link=resume_link)
+
+    return "Student not found"
+
+@app.route("/preview/<stud_id>")
+def preview_file(stud_id):
+    # Fetch the resume BLOB from the database
+    cursor = db_conn.cursor()
+    cursor.execute(f"SELECT stud_resume FROM Student WHERE stud_id = {stud_id}")
+    resume_data = cursor.fetchone()
+    cursor.close()
+
+    if resume_data:
+        # Save the resume to a temporary file
+        resume_blob = resume_data[0]
+        temp_file_path = f"temp_resume_{stud_id}.pdf"
+
+        with open(temp_file_path, 'wb') as file:
+            file.write(resume_blob)
+
+        # Send the file for download
+        return send_file(temp_file_path, as_attachment=True)
+    else:
+        return "Resume not found"
 
 
 if __name__ == '__main__':
