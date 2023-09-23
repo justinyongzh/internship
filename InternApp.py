e077508c765b0f2b3d16ebded305721813e4aeda
diongzy <86460920+diongzy@users.noreply.github.com>
2023-09-23 18:30:49 +0800

    Update InternApp.py

--- InternApp.py
+++ InternApp.py
@@ -252,7 +252,7 @@ def lecturerViewStudentInfo():
     return render_template('lec_displayStudInfo.html', data=result)
 
 
-@app.route("/studProfile/", methods=['GET', 'POST'])
+@app.route("/studProfile", methods=['GET', 'POST'])
 def GetStudInfo():
     username = session.get('username')
 
@@ -263,23 +263,35 @@ def GetStudInfo():
         cursor.execute(statement, (username,))
         student_data = cursor.fetchone()
         cursor.close()
-        
-        return render_template('studProfile.html', student=student_data)
-    else:
-        return "Student not found"
+    
+        if student_data:
+            # Extract student information
+            # stud_id, stud_name, stud_gender, stud_IC, stud_email, stud_HP, stud_address, stud_programme, stud_resume = student_data
+    
+            # Create a link to download the resume
+            resume_link = f"/preview/"
+
+            return render_template('studProfile.html', student=student_data, resume_link=resume_link)
+    
+            # return render_template('studProfile.html', stud_id=stud_id, stud_name=stud_name, stud_gender=stud_gender,
+            #                        stud_IC=stud_IC, stud_email=stud_email, stud_HP=stud_HP, stud_address=stud_address,
+            #                        stud_programme=stud_programme, resume_link=resume_link)
+
+    return "Student not found"
 
 @app.route("/preview/<stud_id>")
 def preview_file(stud_id):
     # Fetch the resume BLOB from the database
+    statement = "SELECT stud_resume FROM Student WHERE stud_id = %s"
     cursor = db_conn.cursor()
-    cursor.execute(f"SELECT stud_resume FROM Student WHERE stud_id = {stud_id}")
+    cursor.execute(statement, (stud_id,))
     resume_data = cursor.fetchone()
     cursor.close()
 
     if resume_data:
         # Save the resume to a temporary file
         resume_blob = resume_data[0]
-        temp_file_path = f"temp_resume_{stud_id}.pdf"
+        temp_file_path = "temp_resume_" + stud_id + ".pdf"
 
         with open(temp_file_path, 'wb') as file:
             file.write(resume_blob)
