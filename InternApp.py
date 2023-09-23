755326fbf73d8548d137c27326825e7549f79f14
diongzy <86460920+diongzy@users.noreply.github.com>
2023-09-24 00:06:43 +0800

    Update InternApp.py

--- InternApp.py
+++ InternApp.py
@@ -306,33 +306,35 @@ def viewCompanyInfo():
     return render_template('admin.html', data=result)
 
 
-@app.route('/delete_company', methods=['POST'])
-def delete_company():
-    company_id = request.form.get('company_id')
+@app.route('/delete_company/<com_id>', methods=['POST'])
+def delete_company(com_id):
+    # company_id = request.form.get('company_id')
     statement = "DELETE FROM company WHERE com_id = %s;"
     cursor = db_conn.cursor()
-    cursor.execute(statement, (company_id,))
+    cursor.execute(statement, (com_id,))
     db_conn.commit()
 
     # Fetch the updated data from the database
-    cursor.execute('SELECT * FROM company WHERE status = 0')
+    statement = "SELECT * FROM company WHERE status = 0"
+    cursor.execute(statement)
     updated_data = cursor.fetchall()
     cursor.close()
 
     # Return the updated data as JSON
     return jsonify(data=updated_data)
 
-@app.route('/update_company_status', methods=['PUT'])
-def update_company_status():
+@app.route('/update_company_status/<com_id>', methods=['PUT'])
+def update_company_status(com_id):
     try:
-        company_id = request.form.get('company_id')
+        # company_id = request.form.get('company_id')
         statement = "UPDATE company SET status = 1 WHERE com_id = %s;"
         cursor = db_conn.cursor()
-        cursor.execute(statement, (company_id,))
+        cursor.execute(statement, (com_id,))
         db_conn.commit()
 
         # Fetch the updated data from the database
-        cursor.execute('SELECT * FROM company WHERE status = 0')
+        statement = "SELECT * FROM company WHERE status = 0"
+        cursor.execute(statement)
         updated_data = cursor.fetchall()
         cursor.close()
 
